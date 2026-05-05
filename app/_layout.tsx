0821b822a416fe7e074d4775aa80a6b4a878ecb7
Ali Syauqie <alisyeager@gmail.com>
2026-05-05 20:16:59 +0700

    Logged in capability

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,40 +1,72 @@
-import { Stack } from 'expo-router';
-import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold } from '@expo-google-fonts/inter';
-import { useEffect } from 'react';
-import * as SplashScreen from 'expo-splash-screen';
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { useAuthContext } from "./_hooks/use-auth-context";
+import AuthProvider from "./_providers/auth-provider";
 
 // Prevent auto hide
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const [loaded] = useFonts({
-    'Inter-Regular': Inter_400Regular,
-    'Inter-SemiBold': Inter_600SemiBold,
-    'Inter-Bold': Inter_700Bold,
-  });
+export function RootLayout() {
+     console.log("le");
+     const { isLoggedIn } = useAuthContext();
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+     const [loaded] = useFonts({
+          "Inter-Regular": Inter_400Regular,
+          "Inter-SemiBold": Inter_600SemiBold,
+          "Inter-Bold": Inter_700Bold,
+     });
 
-  if (!loaded) {
-    return null;
-  }
+     useEffect(() => {
+          if (loaded) {
+               SplashScreen.hideAsync();
+          }
+     }, [loaded]);
 
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="(auth)/login" />
-      <Stack.Screen name="(tabs)" />
-      
-      {/* Modals and other screens */}
-      <Stack.Screen name="payment" options={{ presentation: 'modal' }} />
-      <Stack.Screen name="virtual-key" options={{ presentation: 'modal' }} />
-      <Stack.Screen name="membership" />
-      <Stack.Screen name="top-up" options={{ presentation: 'modal' }} />
-      <Stack.Screen name="notifications" />
-    </Stack>
-  );
+     if (!loaded) {
+          return null;
+     }
+
+     console.log(isLoggedIn);
+
+     return (
+          <Stack screenOptions={{ headerShown: false }}>
+               <Stack.Protected guard={isLoggedIn}>
+                    {/* <Stack.Screen name="index" /> */}
+                    <Stack.Screen name="(tabs)" />
+                    {/* Modals and other screens */}
+                    <Stack.Screen
+                         name="payment"
+                         options={{ presentation: "modal" }}
+                    />
+                    <Stack.Screen
+                         name="virtual-key"
+                         options={{ presentation: "modal" }}
+                    />
+                    <Stack.Screen name="membership" />
+                    <Stack.Screen
+                         name="top-up"
+                         options={{ presentation: "modal" }}
+                    />
+                    <Stack.Screen name="notifications" />
+               </Stack.Protected>
+               <Stack.Protected guard={!isLoggedIn}>
+                    <Stack.Screen name="(auth)/login" />
+               </Stack.Protected>
+          </Stack>
+     );
+}
+
+export default function RootFR() {
+     return (
+          <AuthProvider>
+               <RootLayout />
+          </AuthProvider>
+     );
 }
